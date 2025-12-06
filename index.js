fa21520f98d5c54cbc2613f52b75136fe35eb7df
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-12-06 21:39:15 +0600

    created the project and implement add user function

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.avcddas.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("assignment_11_DB");
+    const usersCollection = db.collection("users");
+
+    // Users Related APIs
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const email = userInfo.userEmail;
+      const isUserExists = await usersCollection.findOne({ userEmail: email });
+
+      if (isUserExists) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("assignment-11");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
